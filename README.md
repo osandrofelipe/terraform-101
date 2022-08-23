b29a8eec6d4b1b206905f7969e6c90d8e969d6b2
Sandro Felipe <sandrokbca@hotmail.com>
2022-08-23 13:06:52 -0300

    bootstrap - Apache

---
+++ README.md
@@ -0,0 +1,1 @@
+# Estudos Terraform
---
+++ scripts/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+    hostnamectl set-hostname apache-terraform
+    apt-get update
+    apt-get install apache2 -y
+    systemctl start apache2
+    systemctl enable apache2
+    echo -e "<h1>Deployed by Terraform</h1>\nYou're on host: $(hostname)" > /var/www/html/index.html
